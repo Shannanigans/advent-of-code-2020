a120d85c735310d1e3d90b1a5b81f0eba43328da
Shannon I'Ons <shannnon@dataprophet.com>
2020-12-08 09:25:38 +0200

    Chore: Duanes day 5 binary answer.

---
+++ day-5-duane.py
@@ -0,0 +1,21 @@
+with open("day-5-data.txt", "r") as file:
+    raw = file.read().strip()
+
+print("raw", raw)
+
+clean = (
+    raw.replace("F", "0")
+    .replace("B", "1")
+    .replace("L", "0")
+    .replace("R", "1")
+    .split("\n")
+)
+
+print("CLEAN", clean)
+
+seats = [int(s, 2) for s in clean]
+print("SEATS", seats)
+print(max(seats))
+seats.sort()
+me = [s - 1 for i, s in enumerate(seats[1:]) if seats[i] < s - 1]
+print(me)
