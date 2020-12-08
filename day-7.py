e916a2865df707303f257947eefc28a51eeb514a
Shannon I'Ons <shannnon@dataprophet.com>
2020-12-08 21:27:27 +0200

    feat: Day 7 WIP.

--- day-7.py
+++ day-7.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from pprint import pprint
 
 
 def get_data_string(filename="day-7-data.txt"):
@@ -14,14 +15,31 @@ multi_replace = lambda string, char_list: reduce(
     lambda result, item: result.replace(item, ""), char_list, string
 )
 
-for x in get_data_string():
-    key, values_string = x.split("contain")
-    key = clean_string(key)
-    full_dict = {
-        clean_string(key): {
+
+def get_map():
+    all_maps = {}
+    for x in get_data_string():
+        key, values_string = x.split("contain")
+        all_maps[clean_string(key)] = {
             k: int(v) if v.isnumeric() else 0
             for child in values_string.split(", ")
             for (k, v) in [split_index(clean_string(child), 1)]
         }
-    }
-    print(full_dict)
+    return all_maps
+
+
+def build_tree(rule_map, key, result={}):
+    result[key] = rule_map[key]
+    for child in result[key]:
+        if child:
+            result[key][child] = build_tree(rule_map, child, result[key])
+        else:
+            result[key] = None
+    return result[key]
+
+
+pprint(build_tree(get_map(), "light salmon"))
+
+# for item in get_map():
+#     print(build_tree(get_map(), item))
+# pprint(build_tree(get_map(), item))
